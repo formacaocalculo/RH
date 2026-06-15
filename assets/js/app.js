7e008c6a2c421a5aeb1cad72e2d6b3debe6a528e
formacaocalculo <martinshernani@outlook.pt>
2026-06-15 19:23:23 +0100

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,19 @@
 // assets/js/app.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { firebaseConfig } from './firebase-config.js';
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-// Escuta a autenticação
+// Escuta a autenticação e faz o redirecionamento
 onAuthStateChanged(auth, (user) => {
+    // ESTA É A LINHA DO LOG PARA DEBUGAR
+    console.log("Estado de autenticação mudou. Utilizador detetado:", user); 
+
     if (user) {
         // Se logado, vai para o dashboard
         window.router.navigate('dashboard');
--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,10 +1,10 @@
 // assets/js/firebase-config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyC6rApsTwViPSVS3EoIo699IoKfWXknOu4",
-  authDomain: "rh-amber.firebaseapp.com",
-  projectId: "rh-amber",
-  storageBucket: "rh-amber.firebasestorage.app",
-  messagingSenderId: "720867955839",
-  appId: "1:720867955839:web:3eece5c5295cffd7c45b4a",
-  measurementId: "G-M0G5KM0QPH"
+export const firebaseConfig = {
+    apiKey: "AIzaSyC6rApsTwViPSVS3EoIo699IoKfWXknOu4",
+    authDomain: "rh-amber.firebaseapp.com",
+    projectId: "rh-amber",
+    storageBucket: "rh-amber.firebasestorage.app",
+    messagingSenderId: "720867955839",
+    appId: "1:720867955839:web:3eece5c5295cffd7c45b4a",
+    measurementId: "G-M0G5KM0QPH"
 };
